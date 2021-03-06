cab4b6e1eb52a6e406250bbe9ad36829c3222109
Pablo Poggio <pablopoggiog@gmail.com>
2021-03-06 15:20:54 -0300

    Create the Meet component for SideBars

---
+++ src/components/SideBar/Meet/data.js
@@ -0,0 +1,6 @@
+import { Inbox as InboxIcon, Star as StarIcon } from "@material-ui/icons";
+
+export const data = [
+  { icon: <InboxIcon />, text: "Nueva reunión" },
+  { icon: <StarIcon />, text: "Unirte a una reunión" },
+];
---
+++ src/components/SideBar/Meet/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  width: 100%;
+`;
+
+export const Title = styled.p`
+  font-size: 13px;
+  margin: 12px 26px;
+  font-weight: 500;
+`;
